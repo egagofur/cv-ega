7339dc1b87dd999d4639be7d1cb09a8d1d475a8a
egagofur <egagofurtriwahana@gmail.com>
2024-07-22 18:24:49 +0700

    fix: font size

--- src/components/footer-card.tsx
+++ src/components/footer-card.tsx
@@ -4,7 +4,7 @@ import WordPullUp from "./ui/word-pull-up";
 
 export function FooterCard() {
   return (
-    <div className="flex  flex-col items-start min-w-full mx-auto relative h-[18rem]">
+    <div className="flex flex-col items-start min-w-full mx-auto relative h-[12rem] md:h-[18rem]">
       <BackgroundGradientAnimation>
         <div className="absolute z-50 inset-0 flex flex-col items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl">
           <WordPullUp
